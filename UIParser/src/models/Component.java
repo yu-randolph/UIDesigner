5111be471871eec2dbcc0e80d28c82172b9c7181
Randolph's MAC <>
2017-11-05 18:30:40 +0800

    Created a Dialog for importing JSON and handlers

---
+++ UIParser/src/models/Component.java
@@ -0,0 +1,12 @@
+package models;
+
+public class UIElement {
+    private String type;
+    private int width;
+    private int height;
+    private int top;
+    private int left;
+    public UIElement(){
+
+    }
+}
